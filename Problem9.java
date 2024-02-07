3cc2e2bc04ed827c3e40316e72b68f0318296419
swami raj <76578276+swami-raj@users.noreply.github.com>
2024-02-07 23:57:59 +0530

    Create Problem9.java

---
+++ Problem9.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Problem9 {
+    public static void main(String[] args) {
+        String str1 = "coding";
+        String str2 = "ingodc";
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+        boolean isAnagram = Arrays.equals(charArray1, charArray2);
+        if (isAnagram) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
